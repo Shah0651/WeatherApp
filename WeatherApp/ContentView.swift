2ef26702898d72cc40a3605293996c8163896962
Shahnawaz <mdshahnawazul@gmail.com>
2024-07-11 15:39:50 +0530

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by MD SHAHNAWAZUL HAQUE on 11/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by MD SHAHNAWAZUL HAQUE on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
